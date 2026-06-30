1d4c37ac6817083b62010c607f993e91a60c922c
01BARRETO1 <karbar_1990@hotmail.com>
2026-06-30 04:27:18 -0500

    Pagina diseñada mediante un prompt

---
+++ menu.js
@@ -0,0 +1,26 @@
+const cards=document.querySelectorAll(".card");
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity="1";
+entry.target.style.transform="translateY(0)";
+
+}
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+card.style.transform="translateY(50px)";
+card.style.transition="1s";
+
+observer.observe(card);
+
+});
